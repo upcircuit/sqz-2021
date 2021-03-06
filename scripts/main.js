ff87c008c4ea563b3b9828b8a195d0919bae615b
Jison <joaquin.jison@gmail.com>
2021-03-06 15:21:26 +0800

    Added layout for reg form moodles.

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,21 @@
 $( document ).ready(function() {
-  console.log("document ready");
   $(".nav-link").on("mouseenter", function(){
     $(this).children(".nav-link-bold").removeClass("hidden-text");
   });
   $(".nav-link").on("mouseleave", function(){
     $(this).children(".nav-link-bold").addClass("hidden-text");
+  });
+
+  //REG FORM JS
+  //in order to transition properly, need to remove disabled (display:none)
+  //BEFORE removing hidden (opacity:0)
+  $("#college-select").on("click", function(){
+    $("#bg-1").removeClass("disabled");
+    $("#college-form").removeClass("disabled");
+    setTimeout(function(){
+      $("#bg-1").removeClass("hidden");
+      $("#college-form").removeClass("hidden");
+    }, 50);
+
   });
 });
