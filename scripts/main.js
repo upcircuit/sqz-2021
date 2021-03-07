bc884fccb026d40c02027f4de0a87decbaf05f5f
Jison <joaquin.jison@gmail.com>
2021-03-07 15:32:31 +0800

    Mass typo fix on stylesheets and scripts.

--- scripts/main.js
+++ scripts/main.js
@@ -12,57 +12,57 @@ $( document ).ready(function() {
   //in order to transition properly, need to remove disabled (display:none)
   //BEFORE removing hidden (opacity:0)
 
-  active_moodle = 1;
+  active_modal = 1;
 
   $("#college-select").on("click", function(){
     $("#bg-1").removeClass("disabled");
     $("#college-form").removeClass("disabled");
-    $("#college-"+active_moodle).removeClass("disabled");
+    $("#college-"+active_modal).removeClass("disabled");
     setTimeout(function(){
       $("#bg-1").removeClass("hidden");
       $("#college-form").removeClass("hidden");
-      $("#college-"+active_moodle).removeClass("hidden");
-      $("#college-"+active_moodle).addClass("active");
+      $("#college-"+active_modal).removeClass("hidden");
+      $("#college-"+active_modal).addClass("active");
     }, 50);
   });
 
   $(".next").on("click", function(){
-    if (active_moodle==5){
+    if (active_modal==5){
       return;
     }
     //push down current active
-    $("#college-"+String(active_moodle)).addClass("phase-out");
-    $("#college-"+String(active_moodle)).removeClass("active");
+    $("#college-"+String(active_modal)).addClass("phase-out");
+    $("#college-"+String(active_modal)).removeClass("active");
     //elevate hidden next page and enable
-    $("#college-"+String(active_moodle+1)).addClass("active");
-    $("#college-"+String(active_moodle+1)).removeClass("disabled");
+    $("#college-"+String(active_modal+1)).addClass("active");
+    $("#college-"+String(active_modal+1)).removeClass("disabled");
     setTimeout(function(){
-      $("#college-"+String(active_moodle+1)).removeClass("hidden");
+      $("#college-"+String(active_modal+1)).removeClass("hidden");
       //disable non-visible formerly active page
-      $("#college-"+String(active_moodle)).addClass("disabled");
-      $("#college-"+String(active_moodle)).addClass("hidden");
-      $("#college-"+String(active_moodle)).removeClass("phase-out");
-      active_moodle +=1;
+      $("#college-"+String(active_modal)).addClass("disabled");
+      $("#college-"+String(active_modal)).addClass("hidden");
+      $("#college-"+String(active_modal)).removeClass("phase-out");
+      active_modal +=1;
     }, 50);
   });
 
   $(".back").on("click", function(){
-    if (active_moodle==1){
+    if (active_modal==1){
       return;
     }
     //push down current active
-    $("#college-"+String(active_moodle)).addClass("phase-out");
-    $("#college-"+String(active_moodle)).removeClass("active");
+    $("#college-"+String(active_modal)).addClass("phase-out");
+    $("#college-"+String(active_modal)).removeClass("active");
     //elevate hidden next page and enable
-    $("#college-"+String(active_moodle-1)).addClass("active");
-    $("#college-"+String(active_moodle-1)).removeClass("disabled");
+    $("#college-"+String(active_modal-1)).addClass("active");
+    $("#college-"+String(active_modal-1)).removeClass("disabled");
     setTimeout(function(){
-      $("#college-"+String(active_moodle-1)).removeClass("hidden");
+      $("#college-"+String(active_modal-1)).removeClass("hidden");
       //disable non-visible formerly active page
-      $("#college-"+String(active_moodle)).addClass("disabled");
-      $("#college-"+String(active_moodle)).addClass("hidden");
-      $("#college-"+String(active_moodle)).removeClass("phase-out");
-      active_moodle -=1;
+      $("#college-"+String(active_modal)).addClass("disabled");
+      $("#college-"+String(active_modal)).addClass("hidden");
+      $("#college-"+String(active_modal)).removeClass("phase-out");
+      active_modal -=1;
     }, 50);
   });
 
